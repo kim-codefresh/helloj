e6a0feb079878520730261a4de2e74bae717c50d
kim-codefresh <kim.aharfi@codefresh.io>
2022-04-17 15:01:57 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,8 @@
-import java.lang.Thread;
 
 public class HelloWorld
 {
     public static void main(String[] args)
     {
-        Thread.sleep(1000);
         System.out.println("Hello, World!");
     }
 }
